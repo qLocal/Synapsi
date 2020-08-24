403e6d8f80b69253d8779d84fccb159c9b844d92
qLocal <69441630+qLocal@users.noreply.github.com>
2020-08-24 17:35:00 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,7 +1,6 @@
-function testFunction()
-    for _,__ in pairs(workspace:GetChildren())do
-        if __.Name == 'Terrain' then
-            print('Found Terrain')
-        end
+for _,__ in pairs(workspace:GetChildren())do
+    if __.Name == 'Terrain' then
+        print('Found Terrain')
     end
 end
+
