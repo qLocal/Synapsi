0418ccee484c2d1ef7c3f62d5446c17058add5bb
qLocal <69441630+qLocal@users.noreply.github.com>
2020-08-25 16:19:01 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,20 +1,12 @@
-for _,__ in pairs(workspace:GetChildren())do
-    if __.Name == 'Terrain' then
-        print('Found Terrain')
-    end
-end
+game:GetService('UserInputService').InputBegan:Connect(function(input, gameProcessed)
+        print(input.KeyCode)
+end)
 
- function printChildren()
-    for i,v in pairs(workspace:GetChildren())do
-        if v:IsA('BasePart') or v:IsA('Model') then
-            print(v.Name)
-            end
+function input()
+    game:GetService('UserInputService').InputBegan:Connect(function(input, gameProcessed)
+        print(input.KeyCode)
         end
-    end
-
-if workspace:FindFirstChild('Baseplate') then
-    print('found baseplate ok')
-    else
-    print(nil)
+    end)
 end
-printChildren()
+
+input()
