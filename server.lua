c8f791dcd40a8bd8578cf02e288488ba55138097
qLocal <69441630+qLocal@users.noreply.github.com>
2020-11-25 17:21:14 +0000

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,16 +1,3 @@
-local f = {}
-
-f['printPlayers'] = function()
-        for i,player in pairs(game.Players:GetChildren())do
-                print(player.Name)
-         end
-end
-for i,v in pairs(f)do
-        table.insert(functions, v)
-end
-
-for i,v in pairs(modules) do
-        print(v)
-        end
-
-return f
+game.Players.PlayerAdded:Connect(function(player)
+	print(player.Name)
+end)
