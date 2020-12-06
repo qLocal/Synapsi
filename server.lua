66dff917c2356e65242d91875d74a825021577e9
qLocal <69441630+qLocal@users.noreply.github.com>
2020-12-06 13:45:38 +0000

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,12 +2,9 @@ local functions = {client = {}, server = {}}
 
 local branches = {functionsModule = require(game.ReplicatedStorage:WaitForChild('Libraries').Functions)}
 
-functions.client.inputBegan = {
-    func = game:GetService('UserInputService').InputBegan:Connect(function(input, isTyping)
-	if isTyping then return end
-	print(input)
-    end),
-    name = 'inputBegan'
-}
+functions.client['inputBegan'] = game:GetService('UserInputService').InputBegan:Connect(function(input, isTyping)
+    if isTyping then return end
+    print(input)
+end)
 	 
 branches.functionsModule.self:unpackClient(functions.client)
