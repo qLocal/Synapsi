0ed221b8f484af9c4958a85e97b9183e610f620c
qLocal <69441630+qLocal@users.noreply.github.com>
2020-12-06 12:11:22 +0000

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,15 @@
-game.Players.PlayerAdded:Connect(function(player)
-	print(player.Name)
-end)
+local functions = {client = {}, server = {}}
+
+local branches = {functionsModule = require(game.ReplicatedStorage:WaitForChild('Libraries').Functions}
+
+functions.client.inputBegan = function()
+	game:GetService('UserInputService').InputBegan:Connect(function(input, isTyping)
+			if not isTyping then
+				print(input)
+			end
+	end)
+end
+	
+branches.functionsModule:unpackClient(functions.client)
+	
+branches.functionsModule:unpackServer(functions.server)
