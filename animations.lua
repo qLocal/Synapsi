cf162b516fe36cb3eba812b94b24245be80c3cf1
qLocal <69441630+qLocal@users.noreply.github.com>
2020-12-13 22:07:04 +0000

    Update animations.lua

--- animations.lua
+++ animations.lua
@@ -1,4 +1,4 @@
-local dictionary = require(game.ReplicatedStorage:WaitForChild('Libraries').Dictionary)
+local dictionary = require(game.ReplicatedStorage:WaitForChild('Modules').Dictionary)
 
 dictionary.animations = {
     {name = 'at_ease', animationId = 0, walkSpeed = 0, stationary = false},
